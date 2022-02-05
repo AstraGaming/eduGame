216c8c7ae9ea28f605a189aaca28b353ca53c9ef
unknown <virginiacampayo@gmail.com>
2022-02-05 22:41:09 +0100

    Página principal index.html montada (solo estilo)

---
+++ js/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+import * as funcLog from "./funcionalidadLogin.js";
+
+var d = document;
+
+window.onload = () =>{
+
+    // Activa el dinamismo del login --> Importante activarlo cada vez que pintemos el login (¡Son inseparables!).
+    funcLog.activarFuncLogin();
+
+    
+};
