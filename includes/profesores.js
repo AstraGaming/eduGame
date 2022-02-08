79bd4268e785f6e3c8adb5c238cc137a003a2574
AstraGaming <99096336+AstraGaming@users.noreply.github.com>
2022-02-08 17:29:34 +0100

    Update profesores.js
    añadimos apellidos a los profesores.

--- includes/profesores.js
+++ includes/profesores.js
@@ -33,10 +33,12 @@ const profesores = collection(db, "profesores");
 
 //Crear dato en la lista profesores.
 /* LO DEJAREMOS PARA LA PARTE DE ADMIN */
-export const crearProfesor = async (nombre, email, fecha, id, rol) => {
+export const crearProfesor = async (nombre, ape1, ape2, email, fecha, id, rol) => {
     array = new Array("");
     const nuevoProfesor = {
         nombre: nombre,
+        apellido1: ape1,
+        apellido2: ape2,
         email: email,
         fCreacion: fecha,
         rol: rol,
@@ -56,10 +58,11 @@ export const verProfesor = async (id) => {
     );
     const profesor = await getDocs(consulta);
     var nombre=profesor.docs[0].data().nombre;
+    var apellidos=profesor.docs[0].data().apellido1+" "+profesor.docs[0].data().apellido2;
     var email=profesor.docs[0].data().email;
     var fCreacion=profesor.docs[0].data().fCreacion;
 
-    console.log(nombre+" "+email+" "+fCreacion);
+    console.log(nombre+" "+apellidos+" "+email+" "+fCreacion);
 }
 //Editar datos profesor.
 export const editarProfesor = async (id,nombre) =>{
